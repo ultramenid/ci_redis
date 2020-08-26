201bc888345c85cb7ae41bb68d3c6b60c948308a
ultramenid <malichamdan@gmail.com>
2020-08-26 18:27:50 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://165.22.50.200/';
+$config['base_url'] = 'http://loadbalancerIpaddress/';
 
 /*
 |--------------------------------------------------------------------------
